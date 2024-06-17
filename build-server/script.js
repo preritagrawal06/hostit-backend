2e72d2742f2a82a0da7d38150f272bc8dcc05e13
preritagrawal06 <agrawalprerit780@gmail.com>
2024-06-17 20:27:03 +0530

    dockerfile update

--- build-server/script.js
+++ build-server/script.js
@@ -3,11 +3,23 @@ const path = require('path')
 const fs = require('fs')
 const {S3Client, PutObjectCommand} = require('@aws-sdk/client-s3')
 const mime = require('mime-types')
-const Redis = require('ioredis')
 require('dotenv').config()
+const {Kafka} = require('kafkajs')
 
+const kafka = new Kafka({
+    clientId: `docker-build-server-${process.env.DEPLOYEMENT_ID}`,
+    brokers:[process.env.KAFKA_BROKER],
+    ssl:{
+        ca:[fs.readFileSync(path.join(__dirname, 'kafka.pem'), 'utf-8')]
+    },
+    sasl:{
+        username: process.env.KAFKA_USER,
+        password: process.env.KAFKA_PASSWORD,
+        mechanism:"plain"
+    }
+})
 
-const publisher = new Redis(process.env.REDIS_URI)
+const producer = kafka.producer()
 
 const s3Client = new S3Client({
     region:process.env.REGION,
@@ -18,34 +30,41 @@ const s3Client = new S3Client({
 })
 
 const PROJECT_ID = process.env.PROJECT_ID
+const DEPLOYEMENT_ID = process.env.DEPLOYEMENT_ID
 
-const publishLog = (log)=>{
-    publisher.publish(`log:${PROJECT_ID}`, JSON.stringify({log}))   
+const publishLog = async (log)=>{
+    await producer.send({topic:"container-logs", messages:[{key:"logs", value: JSON.stringify({PROJECT_ID, DEPLOYEMENT_ID, log})}]})
+}
+
+const updateDeployment = async(status)=>{
+    await producer.send({topic:"deployment-update", messages:[{key: "update", value: JSON.stringify({PROJECT_ID, DEPLOYEMENT_ID, status})}]})
 }
 
 async function init(){
+    await producer.connect()
     console.log('Executing script.js....')
-    publishLog('Build Started...')
+    await publishLog('Build Started...')
     const outDirPath = path.join(__dirname, 'output')
     const p = exec(`cd ${outDirPath} && npm install && npm run build`)
 
-    p.stdout.on('data', (data)=>{
+    p.stdout.on('data', async (data)=>{
         console.log(data.toString())
-        publishLog(data.toString())
+        await publishLog(data.toString())
     })
 
-    p.stdout.on('error', (data)=>{
+    p.stdout.on('error', async (data)=>{
         console.log('error:', data.toString())
-        publishLog(`error: ${data.toString()}`)
+        await publishLog(`error: ${data.toString()}`)
+        await updateDeployment("FAIL")
     })
 
     p.on('close', async ()=>{
-        publishLog("Build complete...")
+        await publishLog("Build complete...")
         console.log("Build complete")
         const distDirPath = path.join(__dirname, 'output', 'dist');
         const distDirContents = fs.readdirSync(distDirPath, {recursive: true}) //recursive true traverse the sub folders to find the files
 
-        publishLog("Starting to upload to bucket...")
+        await publishLog("Starting to upload to bucket...")
         for(const file of distDirContents){
 
             const filePath = path.join(distDirPath, file)
@@ -53,10 +72,10 @@ async function init(){
             if(fs.lstatSync(filePath).isDirectory()) continue;
 
             console.log("Uploading ", filePath);
-            publishLog(`Uploading ${file}`)
+            await publishLog(`Uploading ${file}`)
             
             const command = new PutObjectCommand({
-                Bucket:'hostit-outputs',
+                Bucket:'hostit-outputs-new',
                 Key:`__outputs/${PROJECT_ID}/${file}`,
                 Body: fs.createReadStream(filePath),
                 ContentType: mime.lookup(filePath)
@@ -65,10 +84,12 @@ async function init(){
             await s3Client.send(command)
 
             console.log("Uploaded ", filePath)
-            publishLog(`Uploaded ${file}`)
+            await publishLog(`Uploaded ${file}`)
         }
         console.log('done..')
-        publishLog("Done...")
+        await publishLog("Done...")
+        await updateDeployment("READY")
+        process.exit(0)
     })
 }
 
