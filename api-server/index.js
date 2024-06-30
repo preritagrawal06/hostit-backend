ae3567f49a053e2bafeff3a7e95e2d56e7003e19
preritagrawal06 <agrawalprerit780@gmail.com>
2024-07-01 00:01:20 +0530

    error handling for kafka and clickhouse connection

--- api-server/index.js
+++ api-server/index.js
@@ -38,40 +38,42 @@ const userRouter = require('./Routers/user.routes')
 app.use('/api/v1/user', userRouter)
 
 const initKafkaConsumer = async()=>{
-    await consumer.connect()
-    await consumer.subscribe({topic:"container-logs"})
-    await consumer.run({
-        autoCommit: false,
-        eachBatch: async function ({batch, heartbeat, commitOffsetsIfNecessary, resolveOffset}) {
-            const messages = batch.messages
-            console.log(`Received ${messages.length} messages..`) 
-            for(const message of messages){
-                const stringMsg = message.value.toString()
-                const {PROJECT_ID, DEPLOYEMENT_ID, log} = JSON.parse(stringMsg)
-                try {
-                    const {query_id} = await chClient.insert({
-                        table:'log_events',
-                        values: [{event_id: uuidv4(), deployment_id: DEPLOYEMENT_ID, log}],
-                        format: 'JSONEachRow'
-                    })
-                    console.log(query_id);
-                    resolveOffset(message.offset)
-                    commitOffsetsIfNecessary(message.offset)
-                    await heartbeat()
-                } catch (error) {
-                    console.log(err);
+    try {
+        await consumer.connect()
+        await consumer.subscribe({topic:"container-logs"})
+        await consumer.run({
+            autoCommit: false,
+            eachBatch: async function ({batch, heartbeat, commitOffsetsIfNecessary, resolveOffset}) {
+                const messages = batch.messages
+                console.log(`Received ${messages.length} messages..`) 
+                for(const message of messages){
+                    const stringMsg = message.value.toString()
+                    const {PROJECT_ID, DEPLOYEMENT_ID, log} = JSON.parse(stringMsg)
+                    try {
+                        const {query_id} = await chClient.insert({
+                            table:'log_events',
+                            values: [{event_id: uuidv4(), deployment_id: DEPLOYEMENT_ID, log}],
+                            format: 'JSONEachRow'
+                        })
+                        console.log(query_id);
+                        resolveOffset(message.offset)
+                        commitOffsetsIfNecessary(message.offset)
+                        await heartbeat()
+                    } catch (error) {
+                        console.log(err);
+                    }
                 }
             }
-        }
-    })
+        })
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
-try {
-    initKafkaConsumer()
-    updateDeployment()
-} catch (error) {
-    console.log(error.message);
-}
+
+initKafkaConsumer()
+updateDeployment()
+
 
 // io.listen(9002, ()=>{console.log('socket server running on port: 9002')})
 app.listen(PORT, ()=>{console.log(`api-server running on port: ${PORT}`)})
