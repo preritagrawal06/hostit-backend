d6cb7d8826de6d18bfbe04cbe0aba1daf9854b0b
preritagrawal06 <agrawalprerit780@gmail.com>
2024-08-04 01:40:40 +0530

    final changes

--- api-server/index.js
+++ api-server/index.js
@@ -21,8 +21,14 @@ const consumer = kafka.consumer({groupId: 'api-server-logs-consumer'})
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: ["*", "https://hostit-frontend.vercel.app", "https://hostit.preritagrawal.in"]
+    origin: ["http://localhost:5173", "https://hostit-frontend.vercel.app", "https://hostit.preritagrawal.in"]
 }))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+  
 const PORT = process.env.PORT || 9000
 
 const projectRouter = require('./Routers/project.routes')
