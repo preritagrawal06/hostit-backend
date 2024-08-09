bba3998a3d789b6f1409aef870ab710ce2482a6c
preritagrawal06 <agrawalprerit780@gmail.com>
2024-08-10 01:19:04 +0530

    minor error handling

--- api-server/Controllers/deploymentController/getLogs.js
+++ api-server/Controllers/deploymentController/getLogs.js
@@ -2,18 +2,25 @@ const chClient = require('../../clickHouseConfig')
 
 const getLogs = async(req, res, next)=>{
     const id = req.params.id
-    const logs = await chClient.query({
-        query: `SELECT event_id, deployment_id, log, timestamp from log_events where deployment_id = {deployment_id: String}`,
-        query_params:{
-            deployment_id: id
-        },
-        format: "JSONEachRow"
-    })
-    const rawLogs = await logs.json()
-    res.json({
-        success: true,
-        logs: rawLogs
-    })
+    try {
+        const logs = await chClient.query({
+            query: `SELECT event_id, deployment_id, log, timestamp from log_events where deployment_id = {deployment_id: String}`,
+            query_params:{
+                deployment_id: id
+            },
+            format: "JSONEachRow"
+        })
+        const rawLogs = await logs.json()
+        res.json({
+            success: true,
+            logs: rawLogs
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            message: "Connection Error"
+        })
+    }
 }
 
 module.exports = getLogs
